83a45095af3bc8225398bcc449984f89aa304bef
Aniket <aniket.tikone@holidayextras.com>
2024-10-19 21:16:20 +0530

    feat: allow new lines as delimiters

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -11,3 +11,5 @@ console.log(add("")) // should return 0 if empty string is passed!
 console.log(add("5"));  // Output: 5
 console.log(add("1,5"));     // Output: 6
 console.log(add("10,20,30")); // Output: 60
+console.log(add("1\n2,3"));   // Output: 6
+console.log(add("1,2\n3,4"));  // 10
