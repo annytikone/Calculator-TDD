2010a8dd9dbbc19bec46689ae61cfb6f9527f8cb
Aniket <aniket.tikone@holidayextras.com>
2024-10-19 21:10:44 +0530

    feat:  Return the Number for a single number.

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,5 +1,8 @@
 function add(number) {
   if (number === "") return 0 // should return 0 if empty string is passed!
+  return parseInt(numbers, 10);
+
 }
 
 console.log(add("")) // should return 0 if empty string is passed!
+console.log(add("5"));  // Output: 5
