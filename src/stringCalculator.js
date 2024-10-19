912809e6d601c0b22cfee1e2ad30ce3a71f7bb1a
Aniket <aniket.tikone@holidayextras.com>
2024-10-19 21:16:11 +0530

    feat: allow new lines as delimiters

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,10 +1,13 @@
-function add(number) {
-  if (number === "") return 0 // should return 0 if empty string is passed!
+function add(numbers) {
+  if (numbers === "") return 0 // should return 0 if empty string is passed!
 
-  const numberArray = numbers.split(",").map(num => parseInt(num, 10));
+  // const numberArray = numbers.split(",").map(num => parseInt(num, 10));
+  const numberArray = numbers.split(/,|\n/).map(num => parseInt(num, 10));
   return numberArray.reduce((sum, num) => sum + num, 0);
 
 }
 
 console.log(add("")) // should return 0 if empty string is passed!
 console.log(add("5"));  // Output: 5
+console.log(add("1,5"));     // Output: 6
+console.log(add("10,20,30")); // Output: 60
