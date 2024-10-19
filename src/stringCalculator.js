ba31f4f6d72648822b3cb68f790f64d67b53dcc5
Aniket <aniket.tikone@holidayextras.com>
2024-10-19 21:24:11 +0530

    feat: dont allow negative numbers

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -10,7 +10,13 @@ function add(numbers) {
   }
 
   const numberArray = numbers.split(delimiter).map(num => parseInt(num, 10));
-  return numberArray.reduce((sum, num) => sum + num, 0);
+  const negatives = numberArray.filter(num => num < 0);
+
+  if (negatives.length > 0) {
+      throw new Error(`Negative numbers not allowed: ${negatives.join(", ")}`);
+  }
+
+  return numberArray.reduce((sum, num) => sum + num, 0);  
 
 }
 
@@ -20,3 +26,14 @@ console.log(add("1,5"));     // Output: 6
 console.log(add("10,20,30")); // Output: 60
 console.log(add("1\n2,3"));   // Output: 6
 console.log(add("1,2\n3,4"));  // 10
+console.log(add("//;\n1;2"));       // Output: 3
+try {
+    console.log(add("1,-2,3"));     // Throws: Negative numbers not allowed: -2
+} catch (e) {
+    console.error(e.message);
+}
+try {
+    console.log(add("//;\n1;-2;3;-4")); // Throws: Negative numbers not allowed: -2, -4
+} catch (e) {
+    console.error(e.message);
+}
